5cbb5b7808e608632b9f4083e227524acf6e7e8b
Lucas Burgardt <58077863+Niip07@users.noreply.github.com>
2022-10-04 19:14:14 -0300

    conexao criada

--- SyspaeUEPG/src/controleConexao/Conexao.java
+++ SyspaeUEPG/src/controleConexao/Conexao.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controleConexao;
-
+import java.sql.*;
 /**
  *
  * @author Lucas
  */
 public class Conexao {
+    private String url;
+    private String user;
+    private String password;
+    private Connection con;
     
+    public Conexao(){
+        url = "jdbc:postgresql://localhost:5432/postgres";
+        user = "postgres";
+        password = "1219";
+        
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão realizada com sucesso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
